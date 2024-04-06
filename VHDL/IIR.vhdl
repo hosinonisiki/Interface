1938c34b3d2c3ab9e188bdb2b74158c8d30254e3
hosinonisiki <hosinoseikatu@hotmail.com>
2024-04-06 20:30:34 +0800

    added pipelined multiplier & added 4SLA IIR due to timing problems

--- VHDL/IIR.vhdl
+++ VHDL/IIR.vhdl
@@ -111,3 +111,120 @@ BEGIN
 
     result <= sumX(0) + (sumY(0)(47) & sumY(0)(42 DOWNTO 0))  WHEN Reset = '0' ELSE (OTHERS => '0');
 END bhvr;
+
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.Numeric_std.ALL;
+
+USE WORK.MyPak_IIR.ALL;
+
+ENTITY IIR_4SLA_4th_order IS
+    GENERIC(
+        coefX : signed_vec_44(0 TO 16);
+        coefY : signed_vec_44(0 TO 3)
+    );
+    PORT(
+        input : IN signed(15 DOWNTO 0);
+        output : OUT signed(15 DOWNTO 0);
+
+        Reset : IN std_logic;
+        Clk : IN std_logic
+    );
+END IIR_4SLA_4th_order;
+
+ARCHITECTURE bhvr OF IIR_4SLA_4th_order IS
+    SIGNAL X : signed(15 DOWNTO 0);
+    SIGNAL bufX : signed(31 DOWNTO 0);
+    SIGNAL Y : signed(43 DOWNTO 0);
+    SIGNAL result : signed(43 DOWNTO 0);
+
+    SIGNAL productX : signed_vec_44(0 TO 16);
+    SIGNAL reg_productX : signed_vec_60(0 TO 16);
+    SIGNAL mult_resultX : signed_vec_60(0 TO 16);
+    -- suppose no more than 16 products are summed up
+    SIGNAL sumX : signed_vec_44(0 TO 16);
+    SIGNAL reg_sumX : signed_vec_44(0 TO 16);
+
+    SIGNAL productY : signed_vec_48(0 TO 3);
+    SIGNAL reg_productY : signed_vec_88(0 TO 3);
+    SIGNAL mult_resultY : signed_vec_88(0 TO 3);
+    SIGNAL sumY : signed_vec_48(0 TO 12);
+    SIGNAL reg_sumY : signed_vec_48(0 TO 12);
+BEGIN
+    PROCESS(Clk)
+    BEGIN
+        IF rising_edge(Clk) THEN
+            bufX <= input * x"7B96";
+            X <= bufX(31) & bufX(29 DOWNTO 15);
+            Y <= result;
+            output <= result(43 DOWNTO 28) + ((14 DOWNTO 0 => '0') & result(27));
+        END IF;
+    END PROCESS;
+
+    Xend : FOR i IN 0 TO 16 GENERATE
+        PROCESS(Clk)
+        BEGIN
+            IF rising_edge(Clk) THEN
+                productX(i) <= ((8 DOWNTO 0 => reg_productX(i)(59)) & reg_productX(i)(59 DOWNTO 25)) + ((42 DOWNTO 0 => '0') & reg_productX(i)(24));
+                sumX(i) <= reg_sumX(i);
+            END IF;
+        END PROCESS;
+        piped_multiplierX : ENTITY WORK.multiplier_signed_2stage_piped GENERIC MAP(
+            half_word_length_A => 22,
+            half_word_length_B => 8
+        )PORT MAP(
+            A => coefX(i),
+            B => X,
+            P => mult_resultX(i),
+            Clk => Clk
+        );
+        reg_productX(i) <= mult_resultX(i) WHEN Reset = '0' ELSE (OTHERS => '0');
+        Xsum : IF i /= 16 GENERATE
+            reg_sumX(i) <= sumX(i + 1) + productX(i);
+        END GENERATE Xsum;
+    END GENERATE Xend;
+    reg_sumX(16) <= productX(16) WHEN Reset = '0' ELSE (OTHERS => '0');
+
+    Yend : FOR i IN 0 TO 3 GENERATE
+        PROCESS(Clk)
+        BEGIN
+            IF rising_edge(Clk) THEN
+                productY(i) <= reg_productY(i)(87 DOWNTO 40) + ((46 DOWNTO 0 => '0') & reg_productY(i)(39));
+            END IF;
+        END PROCESS;
+        piped_multiplierY : ENTITY WORK.multiplier_signed_2stage_piped GENERIC MAP(
+            half_word_length_A => 22,
+            half_word_length_B => 22
+        )PORT MAP(
+            A => coefY(i),
+            B => Y,
+            P => mult_resultY(i),
+            Clk => Clk
+        );
+        reg_productY(i) <= mult_resultY(i) WHEN Reset = '0' ELSE (OTHERS => '0');
+        Ysum : IF i /= 3 GENERATE
+            PROCESS(Clk)
+            BEGIN
+                IF rising_edge(Clk) THEN
+                    sumY(i * 4) <= reg_sumY(i * 4);
+                    sumY(i * 4 + 1) <= reg_sumY(i * 4 + 1);
+                    sumY(i * 4 + 2) <= reg_sumY(i * 4 + 2);
+                    sumY(i * 4 + 3) <= reg_sumY(i * 4 + 3);
+                END IF;
+            END PROCESS;
+            reg_sumY(i * 4) <= sumY(i * 4 + 1) + productY(i);
+            reg_sumY(i * 4 + 1) <= sumY(i * 4 + 2);
+            reg_sumY(i * 4 + 2) <= sumY(i * 4 + 3);
+            reg_sumY(i * 4 + 3) <= sumY(i * 4 + 4);
+        END GENERATE Ysum;
+    END GENERATE Yend;
+    PROCESS(Clk)
+    BEGIN
+        IF rising_edge(Clk) THEN
+            sumY(12) <= reg_sumY(12);
+        END IF;
+    END PROCESS;
+    reg_sumY(12) <= productY(3) WHEN Reset = '0' ELSE (OTHERS => '0');
+
+    result <= sumX(0) + (sumY(0)(47) & sumY(0)(42 DOWNTO 0))  WHEN Reset = '0' ELSE (OTHERS => '0');
+END bhvr;
---
+++ VHDL/multiplier_pipelined.vhdl
@@ -0,0 +1,54 @@
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.Numeric_std.ALL;
+
+ENTITY multiplier_signed_2stage_piped IS
+    GENERIC(
+        half_word_length_A : INTEGER := 8;
+        half_word_length_B : INTEGER := 8
+    );
+    PORT(
+        A : IN signed(2 * half_word_length_A - 1 DOWNTO 0);
+        B : IN signed(2 * half_word_length_B - 1 DOWNTO 0);
+        P : OUT signed(2 * half_word_length_A + 2 * half_word_length_B - 1 DOWNTO 0);
+        Clk : IN std_logic
+    );
+END multiplier_signed_2stage_piped;
+
+ARCHITECTURE bhvr OF multiplier_signed_2stage_piped IS
+    SIGNAL A_high : signed(half_word_length_A - 1 DOWNTO 0);
+    SIGNAL B_high : signed(half_word_length_B - 1 DOWNTO 0);
+    SIGNAL A_low : signed(half_word_length_A DOWNTO 0);
+    SIGNAL B_low : signed(half_word_length_B DOWNTO 0);
+    SIGNAL reg_AhBh : signed(half_word_length_A + half_word_length_B - 1 DOWNTO 0);
+    SIGNAL reg_AhBl, reg_AlBh : signed(half_word_length_A + half_word_length_B DOWNTO 0);
+    SIGNAL reg_AlBl : signed(half_word_length_A + half_word_length_B + 1 DOWNTO 0);
+    SIGNAL AhBh, AhBl, AlBh, AlBl : signed(half_word_length_A + half_word_length_B - 1 DOWNTO 0);
+    SIGNAL buf_sum1, buf_sum2 : signed(2 * half_word_length_A + 2 * half_word_length_B - 1 DOWNTO 0);
+BEGIN
+    PROCESS(Clk)
+    BEGIN
+        IF rising_edge(Clk) THEN
+            AhBh <= reg_AhBh;
+            AlBl <= reg_AlBl(half_word_length_A + half_word_length_B - 1 DOWNTO 0);
+            AhBl <= reg_AhBl(half_word_length_A + half_word_length_B - 1 DOWNTO 0);
+            AlBh <= reg_AlBh(half_word_length_A + half_word_length_B - 1 DOWNTO 0);
+        END IF;
+    END PROCESS;
+
+    A_high <= A(2 * half_word_length_A - 1 DOWNTO half_word_length_A);
+    A_low <= '0' & A(half_word_length_A - 1 DOWNTO 0);
+    B_high <= B(2 * half_word_length_B - 1 DOWNTO half_word_length_B);
+    B_low <= '0' & B(half_word_length_B - 1 DOWNTO 0);
+
+    reg_AhBh <= A_high * B_high;
+    reg_AlBl <= A_low * B_low;
+    reg_AhBl <= A_high * B_low;
+    reg_AlBh <= A_low * B_high;
+
+    buf_sum1 <= (AhBh & (half_word_length_A + half_word_length_B - 1 DOWNTO 0 => '0'))
+                + ((half_word_length_B - 1 DOWNTO 0 => AhBl(half_word_length_A + half_word_length_B - 1)) & AhBl & (half_word_length_A - 1 DOWNTO 0 => '0'));
+    buf_sum2 <= ((half_word_length_A - 1 DOWNTO 0 => AlBh(half_word_length_A + half_word_length_B - 1)) & AlBh & (half_word_length_B - 1 DOWNTO 0 => '0'))
+                + ((half_word_length_A + half_word_length_B - 1 DOWNTO 0 => '0') & AlBl);
+    P <= buf_sum1 + buf_sum2;
+END bhvr;
